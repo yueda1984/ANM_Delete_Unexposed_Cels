4dc73a3bca787c98d33d1b751e5c17fd575fe53b
yueda1984 <61306261+yueda1984@users.noreply.github.com>
2024-04-23 16:47:23 +0900

    Folder structure update

--- src/ANM_Delete_Unexposed_Cels.js
+++ src/ANM_Delete_Unexposed_Cels.js
@@ -16,8 +16,8 @@
 	2) Locate to your user scripts folder (a hidden folder):
 	   https://docs.toonboom.com/help/harmony-17/premium/scripting/import-script.html	
 	   
-	3) Add all unzipped files (*.js and script-icons folder) directly to the folder above.
-	4) Add ANM_Delete_Unexposed_Cels to any toolbar.	
+	3) There is a folder named src inside the zip file. Copy all its contents directly to the folder above.
+	4) In Harmony, add ANM_Delete_Unexposed_Cels function to any toolbar.	
 	
 	
 	Direction:
